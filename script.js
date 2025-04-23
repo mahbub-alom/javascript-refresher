ed72a18ecfe572e66d9f0ba1342b9e39b2089051
mahbub-alom <mdmahbuba034@gmail.com>
2025-04-23 11:05:21 +0600

    function recap done

--- script.js
+++ script.js
@@ -10,8 +10,39 @@
 
 // 2/ function
 
-function hello() {
-  console.log("hello world");
-}
+/*  regular function  */
 
-hello()
+// function hello() {
+//   console.log("hello world");
+// }
+// hello()
+
+/* function expression */
+// const hello = function () {
+//   console.log("hello expression");
+// };
+// console.log(hello);
+
+/* name function expression */
+// const hello = function hello () {
+//   console.log("hello expression");
+// };
+// console.log(hello());
+
+/* arrow function */
+// const hello = () => {
+//   console.log("hello arrow");
+// };
+
+// console.log(hello())
+
+
+/* annoneymouse function */
+
+// const hello(){
+//   return ()=>{
+//     console.log("hello annoneymouse")
+//   }
+// }
+
+// console.log(hello())
