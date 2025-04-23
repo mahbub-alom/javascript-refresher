55dd8cf1c67842b4c68f23c5a91ade4fcd8a6fd1
mahbub-alom <mdmahbuba034@gmail.com>
2025-04-23 10:26:24 +0600

    js scope done

---
+++ script.js
@@ -0,0 +1,17 @@
+//  1/  scope
+// var lang = "bangla";
+// function learn(topic) {
+//   lang = topic;
+//   console.log("this is function call", topic);
+// }
+
+// learn("javascript");
+// console.log("this is public call", lang);
+
+// 2/ function
+
+function hello() {
+  console.log("hello world");
+}
+
+hello()
